6e135b6f29ee1fadcbed0967495f2f8e30318dd2
PetitStrawberry <deloreantaro@yahoo.co.jp>
2025-04-20 15:14:28 +0900

    fix: update service file installation method and improve systemd unit definitions

--- flake.nix
+++ flake.nix
@@ -37,26 +37,29 @@
 
               # Copy systemd service files
               mkdir -p $out/etc/systemd/system
-              cp moused/install/moused.service $out/etc/systemd/system/
-              cp keyboardd/install/keyboardd.service $out/etc/systemd/system/
-              cp tabletmoded/install/tabletmoded.service $out/etc/systemd/system/
+              install -Dm 0644 moused/install/moused.service $out/etc/systemd/system/moused.service
+              install -Dm 0644 keyboardd/install/keyboardd.service $out/etc/systemd/system/keyboardd.service
+              install -Dm 0644 tabletmoded/install/tabletmoded.service $out/etc/systemd/system/tabletmoded.service
             '';
-
-            systemd.units = [
-              "moused.service"
-              "keyboardd.service"
-              "tabletmoded.service"
-            ];
 
             meta = {
               description = "Support package for CHUWI MiniBook";
               homepage = "https://github.com/petitstrawberry/minibook-support";
-              license = "MIT";
+              license = pkgs.lib.licenses.mit;
               platforms = [ "x86_64-linux" ];
             };
-
           };
         }
       );
+
+      nixosModules.default = { config, pkgs, ... }: {
+        systemd.units = {
+          "moused.service".enable = true;
+          "keyboardd.service".enable = true;
+          "tabletmoded.service".enable = true;
+        };
+
+        environment.systemPackages = [ self.packages.${pkgs.system}.default ];
+      };
     };
 }
